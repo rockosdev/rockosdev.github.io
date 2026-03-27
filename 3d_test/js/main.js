5eb0cbcd42bb3bd9c771f9e48ce9620071647151
rock <rock@192.168.2.140>
2026-03-28 01:14:58 +0800

    Update blog 2026-03-28 01:14:58

--- 3d_test/js/main.js
+++ 3d_test/js/main.js
@@ -54,8 +54,8 @@ const __screenForward = new THREE.Vector3();
 const __screenToCamera = new THREE.Vector3();
 
 const MOBILE_OVERVIEW_CAMERA_COMPOSITION = {
-    direction: new THREE.Vector3(0, 0.08, 1),
-    padding: 1.72,
+    direction: new THREE.Vector3(-2, 0.08, 1),
+    padding: 3.72,
     targetNdc: new THREE.Vector2(0, 0),
     enableScreenSpaceCompensation: true
 };
