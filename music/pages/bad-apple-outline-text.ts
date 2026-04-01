e341a884fd17902a64089f1cf2b0c46126f957c8
rock <rock@192.168.2.186>
2026-04-01 23:58:28 +0800

    Update blog 2026-04-01 23:58:27

---
+++ music/pages/bad-apple-outline-text.ts
@@ -0,0 +1,126 @@
+export const ZH_TEXT = `Bad Apple!!
+即便身处流逝的时光之中
+不断的令人疲倦 看吧就这样呼噜呼噜转动着
+我的那颗离去的心
+不被发现的话 那样也能知道吗
+
+就算自己什么也不做
+在时间的缝隙里流逝仍在持续
+什么也不知道 周围的一切事物
+我就是我 仅此而已
+
+梦见了吗 什么也没看见吗
+说什么也没用了的 这自己的话语
+悲伤什么的 也只是徒增疲劳啊
+什么也感觉不到 像这样过下去就好
+
+即便被给予着困惑的话语
+自己的心仍然停留在半空
+如果我试着这样去做的话
+如果一切都将改变的话 那将变成黑暗
+
+这样的自己 能有未来吗
+这样的世界 有我存在吗
+如今难受吗 如今悲伤吗
+连自己的事也不知道的样子
+
+只要继续走下去 就会徒增疲劳啊
+与他人相关的事 装作都不清楚就好了
+即便这样的我也能够改变的话
+如果真的能够改变的话 变得洁白无瑕
+
+即便身处流逝的时光之中
+不断的令人疲倦 看吧就这样呼噜呼噜转动着
+我的那颗离去的心
+不被发现的话 那样也能知道吗
+
+就算自己什么也不做
+在时间的缝隙里流逝仍在持续
+什么也不知道 周围的一切事物
+我就是我 仅此而已
+
+梦见了吗 什么也没看见吗
+说什么也没用了的 这自己的话语
+悲伤什么的 也只是徒增疲劳啊
+什么都感觉不到 像这样过下去就好
+
+即便被给予着困惑的话语
+自己的心仍然停留在半空
+如果我试着这样去做的话
+如果一切都将改变的话 那将变成黑暗
+
+在蹉跎的时间中 未来真的存在吗
+在这样的地方中 我真的存在吗
+关于我的事 如果要说的话
+用言语表达的话 就是『无用之物』
+
+在这样的地方中 我真的存在吗
+在这样的时间中 我真的存在吗
+即便这样的我也能够改变的话
+如果真的能够改变的话 变得洁白无瑕
+
+如今梦见了吗 什么也没看见吗
+说什么也没用了的 这自己的话语
+悲伤什么的 也只是徒增疲劳啊
+什么都感觉不到 像这样过下去就好
+
+即便被给予着困惑的话语
+自己的心仍然停留在半空
+如果我试着这样去做的话
+如果一切都将改变的话 那将变成黑暗
+
+如果试着去做的话 如果试着去做的话
+一切都将崩坏 一切都将崩坏
+如果还能悲伤的话 如果还能悲伤的话
+我的心是否能够变得洁白无瑕
+
+不管是你的事也好还是我的事
+所有的事全都不清楚了
+沉重的眼皮如果睁开的话
+既然一切都崩坏的话 那就干脆化作黑暗吧`
+
+export const EN_TEXT = `Bad Apple!!
+Amid time flowing along, only languidness is circling on and on
+I can't see even my heart being pulled apart from me, don't you ever know?
+
+Without making a move, carried away in between the cracks of time
+All the people around me I don't know; this is who I am and that's all
+
+Am I dreaming? Do I see nothing? Is it just useless to speak my words?
+I've gotten tired of being sad; all I want is to live feeling nothing at all
+If I'm given bewildering remarks, my heart is like in the clouds up above
+
+Is there a future for me? Do I exist in a world like this?
+Wondering if I'm painful or sad, I don't know what's all about me
+I've gotten tired of walking anymore; I don't care what people think about me
+If I can ever change myself, will my heart be able to turn white?
+
+Amid time flowing along, only languidness is circling on and on
+I can't see even my heart being pulled apart from me, don't you ever know?
+
+Without making a move, carried away in between the cracks of time
+All the people around me I don't know; this is who I am and that's all
+
+Am I dreaming? Do I see nothing? Is it just useless to speak my words?
+I've gotten tired of being sad; all I want is to live feeling nothing at all
+If I'm given hesitating words, my heart is like in the clouds up above
+
+If I bring myself to make a move, if I change everything, I'll paint it all black
+
+Is there a future for wasteful time? Do I exist in a place like this?
+If you want to tell about me, if there's a word to describe me it's like "good for nothing"
+
+Do I exist in a place like this? Do I exist in time like this?
+If I can ever change myself, will my heart be able to turn white?
+
+Am I dreaming? Do I see nothing? Is it just useless to speak my words?
+I've gotten tired of being sad; all I want is to live feeling nothing at all
+If I'm given hesitating words, my heart is like in the clouds up above
+
+If I bring myself to make a move, if I change everything, I'll paint it all black
+
+If I make a move, I'll destroy everything for once and for all
+If I feel sad, will my heart be able to turn white?
+
+I don't know about you, me and everything around me
+If I open up my heavy eyelids; if I were to destroy everything, let it all turn black`
